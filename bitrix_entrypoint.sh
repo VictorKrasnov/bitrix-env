10b9d4f37a2acfb1a2f581163215e9445c8c1aa4
Victor Krasnov <krasnovvy@gmail.com>
2019-09-12 19:25:00 +0300

    Update bitrix_entrypoint.sh

--- bitrix_entrypoint.sh
+++ bitrix_entrypoint.sh
@@ -7,10 +7,10 @@ fi
 # configure XDebug
 if [[ -e /etc/php.d/15-xdebug.ini.disabled ]]; then
     echo "zend_extension=xdebug.so"         > /etc/php.d/15-xdebug.ini
-    echo "xdebug.remote_autostart=on"       >> /etc/php.d/15-xdebug.ini
+    echo "#xdebug.remote_autostart=on"       >> /etc/php.d/15-xdebug.ini
     echo "xdebug.remote_enable=on"          >> /etc/php.d/15-xdebug.ini
     echo "xdebug.remote_connect_back=on"    >> /etc/php.d/15-xdebug.ini
-    echo "xdebug.idekey=PHPSTORM"           >> /etc/php.d/15-xdebug.ini
+    echo "#xdebug.idekey=PHPSTORM"           >> /etc/php.d/15-xdebug.ini
     rm /etc/php.d/15-xdebug.ini.disabled
     echo "XDebug enabled"
 fi
